4c321bb82e3a3322e849f4a39fc7dff376c94e52
Marinishka <shipovalova2015@yandex.ru>
2021-06-10 18:14:55 +0300

    Fix

--- src/components/tab-item/tab-item.jsx
+++ src/components/tab-item/tab-item.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const TabItem = ({tabTitle, isTabActive}) => {
   return <li className="tabs__item">
-    <a tabIndex="0" data-tab={tabTitle} href="#" className={`tabs__link ${isTabActive ? `tabs__link--active` : ``}`}>{tabTitle}</a>
+    <a tabIndex="0" data-tab={tabTitle} href="#" className={`tabs__link ${isTabActive ? `tabs__link--active` : ``}`} onClick={(evt) => evt.preventDefault()}>{tabTitle}</a>
   </li>;
 };
 
